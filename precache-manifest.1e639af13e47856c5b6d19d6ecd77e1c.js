cb7a5dd88337969bcd6787b042dedb38aca8cbdf
Ben Alderfer <benalderfer974@gmail.com>
2020-04-02 22:28:19 -0400

    Updates

---
+++ precache-manifest.1e639af13e47856c5b6d19d6ecd77e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f9b64682936d9cea02a9741e8015441",
+    "url": "/division2builder/index.html"
+  },
+  {
+    "revision": "592cb3da412a44b2e90a",
+    "url": "/division2builder/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "0aa540bcce4a72d3db66",
+    "url": "/division2builder/static/js/2.1d13f779.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/division2builder/static/js/2.1d13f779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "592cb3da412a44b2e90a",
+    "url": "/division2builder/static/js/main.e60171bc.chunk.js"
+  },
+  {
+    "revision": "8fbd968b3767bf07dabb",
+    "url": "/division2builder/static/js/runtime-main.9f6a4a9e.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/division2builder/static/media/logo.ee7cd8ed.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/division2builder/precache-manifest.1e639af13e47856c5b6d19d6ecd77e1c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/division2builder/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
