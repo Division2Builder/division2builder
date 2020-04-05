8fb06395dde09886302791671df4f95921b3909b
Michael-Longley <84longleym@gmail.com>
2020-04-05 17:39:35 -0400

    Working on structure of selected items. Added images

--- src/components/gearSelection.js
+++ src/components/gearSelection.js
@@ -5,6 +5,7 @@ import Vest from '../icons/gear_types/vest.png'
 import Glove from '../icons/gear_types/gloves.png'
 import Holster from '../icons/gear_types/holster.png'
 import Kneepads from '../icons/gear_types/kneepads.png'
+import Rifle from '../icons/weapons/Rifle.png'
 import './builder.css'
 
 function GearSelection() {
@@ -14,46 +15,48 @@ function GearSelection() {
                 Placeholder Text
             </div>
             <div className="card gernade">
+                
                 Placeholder Text
             </div>
             <div className="card weapon">
-                Placeholder Text
+                <img className="icon" src={Rifle} alt="primary"/>
+                <span className="name named">Baker's Dozen</span>
             </div>
             <div className="card weapon">
-                Placeholder Text
+                <span className="unselected">Select a Weapon</span>
             </div>
             <div className="card sidearm">
-                Placeholder Text
+                <span className="unselected">Select a Sidearm</span>
             </div>
             <div className="card armor" id="mask">
-                <img className="icon" src={Mask} alt="backpack"/>
-                Placeholder Text
+                <img className="icon" src={Mask} alt="mask"/>
+                <span className="unselected">Select a Mask</span>
             </div>
             <div className="card armor" id="backpack">
                 <img className="icon" src={Backpack} alt="backpack"/>
-                Placeholder Text
+                <span className="name">Yaahl Pack</span>
             </div>
             <div className="card armor" id="vest">
-                <img className="icon" src={Vest} alt="backpack"/>
-                Placeholder Text
+                <img className="icon" src={Vest} alt="vest"/>
+                <span className="unselected">Select a Vest</span>
             </div>
             <div className="card armor" id="glove">
-                <img className="icon" src={Glove} alt="backpack"/>
-                Placeholder Text
+                <img className="icon" src={Glove} alt="glove"/>
+                <span className="unselected">Select a Glove</span>
             </div>
             <div className="card armor" id="holster">
-                <img className="icon" src={Holster} alt="backpack"/>
-                Placeholder Text
+                <img className="icon" src={Holster} alt="holster"/>
+                <span className="unselected">Select a Holster</span>
             </div>
             <div className="card armor" id="kneepads">
-                <img className="icon" src={Kneepads} alt="backpack"/>
-                Placeholder Text
+                <img className="icon" src={Kneepads} alt="kneepads"/>
+                <span className="unselected">Select a KneePad</span>
             </div>
             <div className="card skill" >
-                Placeholder Text
+                <span className="unselected">Select a skill</span>
             </div>
             <div className="card skill" >
-                Placeholder Text
+                <span className="unselected">Select a skill</span>
             </div>
         </div>
     );
