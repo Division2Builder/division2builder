a20408242dd9b606bae7ea24693131ba325dcace
Jake Bashaw <jbashaw36@gmail.com>
2020-04-09 14:04:53 -0400

    More stats modal changes
    Added the section selection and breakdown columns

--- src/components/stats.js
+++ src/components/stats.js
@@ -1,40 +1,155 @@
 import React from 'react';
 import './stats.css'
+import Vest from '../icons/gear_types/vest.png'
+import BreakdownItem from "./breakdownItem";
 
 function Stats() {
     return (
         <div className="statsGrid">
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
+            <div className="grid sectionSelectionGrid">
+                <img className="dsIco" src={Vest} alt="logo"/>
+                <img className="dsIco" src={Vest} alt="logo"/>
+                <img className="dsIco" src={Vest} alt="logo"/>
+                <img className="dsIco" src={Vest} alt="logo"/>
+                <img className="dsIco" src={Vest} alt="logo"/>
+                <img className="dsIco" src={Vest} alt="logo"/>
+                <img className="dsIco" src={Vest} alt="logo"/>
+                <img className="dsIco" src={Vest} alt="logo"/>
             </div>
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
+            <div className="grid statValuesGrid">
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
+                <div className="statSection">
+                    <h6>Section Name</h6>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                    <div className="stat">
+                        <span className="statAmount">123,456</span>
+                        <span className="statName">Stat Name</span>
+                    </div>
+                </div>
             </div>
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
-            </div>
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
-            </div>
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
-            </div>
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
-            </div>
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
-            </div>
-            <div className="card">
-                <span className="statAmount">123,456</span>
-                <span className="statName">Stat Name</span>
+            <div className="grid statBreakdownGrid">
+                <h2>Breakdown</h2>
+                <div className="breakdownDescription">
+                    <h5>Killer</h5>
+                    <p>the description</p>
+                </div>
+                <div className="breakdownItems">
+                    <p>Found on:</p>
+                    <div>
+                        <BreakdownItem/>
+                    </div>
+                </div>
             </div>
         </div>
     );
