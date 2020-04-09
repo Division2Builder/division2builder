55e69ea52e69e25ec8d1f9424a4839d74495bad1
Ben Alderfer <benalderfer974@gmail.com>
2020-04-08 22:23:10 -0400

    Add attrs

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './sidebar.css'
+import Red from '../icons/attrs/red.png'
+import Yellow from '../icons/attrs/yellow.png'
+import Blue from '../icons/attrs/blue.png'
 
 export default function Sidebar() {
     return (
-        <div className="sidebarGrid card">
+        <div className="sidebarGrid flex-column card">
             <div className="circleLevel lineBelow">
                 Circle breakdown
             </div>
-            <div className="attrs lineBelow">
-                Placeholder Text
+            <div className="attrsSection flex-row lineBelow">
+                <div className="flex-column">
+                    <div className="flex-row">
+                        <img src={Red} alt="Red" className="attrsIcon"/>
+                        <div>5</div>
+                    </div>
+                    <div className="flex-row">
+                        <img src={Yellow} alt="Yellow" className="attrsIcon"/>
+                        <div>5</div>
+                    </div>
+                    <div className="flex-row">
+                        <img src={Blue} alt="Blue" className="attrsIcon"/>
+                        <div>5</div>
+                    </div>
+                </div>
+                <div className="flex-column">
+                    <div className="flex-row">
+                        <img src={Red} alt="Red" className="attrsIcon"/>
+                        <div>5</div>
+                    </div>
+                    <div className="flex-row">
+                        <img src={Yellow} alt="Yellow" className="attrsIcon"/>
+                        <div>5</div>
+                    </div>
+                    <div className="flex-row">
+                        <img src={Blue} alt="Blue" className="attrsIcon"/>
+                        <div>5</div>
+                    </div>
+                </div>
             </div>
             <div className="defenseStats lineBelow">
                 <div>Armor</div>
@@ -17,7 +47,10 @@ export default function Sidebar() {
                 <div>123.4k</div>
             </div>
             <div className="skillTier">
-                Placeholder Text
+                <div>Skill Tier</div>
+                <div className="skillIcons">
+
+                </div>
             </div>
         </div>
     );
