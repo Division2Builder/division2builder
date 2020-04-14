c45d8dc8e9e134e3c9732e51292c4406812b0225
Ben Alderfer <benalderfer974@gmail.com>
2020-04-13 23:29:10 -0400

    Fix centering content

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import GearSelection from './components/gearSelection'
 import './App.scss';
 import Background from './components/shared/background.png'
 import Sidebar from "./components/sidebar";
+import {Row} from "react-bootstrap";
 
 function App() {
     return (
         <div className="App" style={style}>
-            <div className="container row appContainer">
-                <Sidebar/>
-                <GearSelection/>
+            <div className="container appContainer">
+                <Row>
+                    <Sidebar/>
+                    <GearSelection/>
+                </Row>
             </div>
         </div>
     );
