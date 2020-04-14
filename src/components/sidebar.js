846adcb8e2d67994c84c8e2ca1a56449e100d547
Ben Alderfer <benalderfer974@gmail.com>
2020-04-13 23:21:09 -0400

    Fix attrs and skill tier

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,10 +1,11 @@
 import React, {useEffect} from 'react';
 import Chart from 'chart.js';
 
-import './sidebar.scss'
-import Red from '../icons/attrs/red.png'
-import Yellow from '../icons/attrs/yellow.png'
-import Blue from '../icons/attrs/blue.png'
+import './sidebar.scss';
+import Red from '../icons/attrs/red.png';
+import Yellow from '../icons/attrs/yellow.png';
+import Blue from '../icons/attrs/blue.png';
+import {Col, Row} from 'react-bootstrap';
 
 export default function Sidebar() {
     useEffect(() => {
@@ -37,53 +38,53 @@ export default function Sidebar() {
                 <canvas id="donutChart"/>
             </div>
             <div className="attrsSection col-xs-12 row lineBelow">
-                <div className="col">
-                    <div className="row">
+                <Col>
+                    <Row className="align-items-center">
                         <img src={Red} alt="Red" className="attrsIcon col-xs-3"/>
-                        <div className="col-xs-3 col-xs-offset-1">5</div>
-                    </div>
-                    <div className="row">
+                        <div className="col-xs-3">5</div>
+                    </Row>
+                    <Row className="align-items-center">
                         <img src={Blue} alt="Blue" className="attrsIcon col-xs-3"/>
-                        <div className="col-xs-3 col-xs-offset-1">5</div>
-                    </div>
-                    <div className="row">
+                        <div className="col-xs-3">5</div>
+                    </Row>
+                    <Row className="align-items-center">
                         <img src={Yellow} alt="Yellow" className="attrsIcon col-xs-3"/>
-                        <div className="col-xs-3 col-xs-offset-1">5</div>
-                    </div>
-                </div>
-                <div className="col">
-                    <div className="row align-items-center">
+                        <div className="col-xs-3">5</div>
+                    </Row>
+                </Col>
+                <Col>
+                    <Row className="align-items-center">
                         <img src={Red} alt="Red" className="attrsIcon col-xs-3"/>
-                        <div className="col-xs-3 col-xs-offset-1">5</div>
-                    </div>
-                    <div className="row">
+                        <div className="col-xs-3">5</div>
+                    </Row>
+                    <Row className="align-items-center">
                         <img src={Blue} alt="Blue" className="attrsIcon col-xs-3"/>
-                        <div className="col-xs-3 col-xs-offset-1">5</div>
-                    </div>
-                    <div className="row">
+                        <div className="col-xs-3">5</div>
+                    </Row>
+                    <Row className="align-items-center">
                         <img src={Yellow} alt="Yellow" className="attrsIcon col-xs-3"/>
-                        <div className="col-xs-3 col-xs-offset-1">5</div>
-                    </div>
-                </div>
+                        <div className="col-xs-3">5</div>
+                    </Row>
+                </Col>
             </div>
-            <div className="defenseStats col-xs-12 lineBelow">
+            <Col xs={12} className="defenseStats lineBelow">
                 <h4>Armor</h4>
                 <h5>123.4k</h5>
                 <h4>Health</h4>
                 <h5>123.4k</h5>
-            </div>
-            <div className="skillTier col-xs-12">
-                <div>Skill Tier</div>
-                <div className="row">
-                    <img src={Yellow} alt="Yellow" className="attrsIcon col"/>
-                    <div className="skillCell col"></div>
-                    <div className="skillCell col"></div>
-                    <div className="skillCell col"></div>
-                    <div className="skillCell col"></div>
-                    <div className="skillCell col"></div>
-                    <div className="skillCell col"></div>
-                </div>
-            </div>
+            </Col>
+            <Col xs={12} className="skillTier">
+                <p>Skill Tier</p>
+                <Row>
+                    <img src={Yellow} alt="Yellow" className="attrsIcon col-xs-1" align="bottom"/>
+                    <Col className="skillCell"/>
+                    <Col className="skillCell"/>
+                    <Col className="skillCell"/>
+                    <Col className="skillCell"/>
+                    <Col className="skillCell"/>
+                    <Col className="skillCell"/>
+                </Row>
+            </Col>
         </div>
     );
 }
