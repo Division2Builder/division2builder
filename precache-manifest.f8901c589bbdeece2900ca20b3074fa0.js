546f90e6d5e343f29d388b5ede660f1b67ef9d73
BenAlderfer <BenAlderfer@users.noreply.github.com>
2020-05-02 18:19:40 +0000

    Deploying to gh-pages from  @ 6779d61c304a1ad974cc8620b19667560f9136e2 🚀

---
+++ precache-manifest.f8901c589bbdeece2900ca20b3074fa0.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b7de34ee98e0e903eeec310b35dfcc",
+    "url": "/division2builder/index.html"
+  },
+  {
+    "revision": "e907eb0f0fe051c21f10",
+    "url": "/division2builder/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "8f454d90b1a69c4a96ab",
+    "url": "/division2builder/static/css/main.397b67cd.chunk.css"
+  },
+  {
+    "revision": "e907eb0f0fe051c21f10",
+    "url": "/division2builder/static/js/2.34641e6f.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/division2builder/static/js/2.34641e6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f454d90b1a69c4a96ab",
+    "url": "/division2builder/static/js/main.0f34f92a.chunk.js"
+  },
+  {
+    "revision": "8fbd968b3767bf07dabb",
+    "url": "/division2builder/static/js/runtime-main.9f6a4a9e.js"
+  },
+  {
+    "revision": "86747d53f71b422bca3a96ea414af8c4",
+    "url": "/division2builder/static/media/AcesAndEights.86747d53.png"
+  },
+  {
+    "revision": "64c568b22d0f6ebab048b43ca2350609",
+    "url": "/division2builder/static/media/Airaldi.64c568b2.png"
+  },
+  {
+    "revision": "dd78239477366c61b7715031502f7d59",
+    "url": "/division2builder/static/media/Borda Bold Italic.dd782394.ttf"
+  },
+  {
+    "revision": "43ef61aefea93b5c749facaf668dff39",
+    "url": "/division2builder/static/media/Borda Bold.43ef61ae.ttf"
+  },
+  {
+    "revision": "dc1ecb28fb785b2d7f0855f01bc2bffc",
+    "url": "/division2builder/static/media/Borda Italic.dc1ecb28.ttf"
+  },
+  {
+    "revision": "cf262d1b98864ac6b0c50513aaee5159",
+    "url": "/division2builder/static/media/Borda Light Italic.cf262d1b.ttf"
+  },
+  {
+    "revision": "47d1b92bc834512ea36e9d3235f0a1ea",
+    "url": "/division2builder/static/media/Borda Light.47d1b92b.ttf"
+  },
+  {
+    "revision": "4a2b71585001d1958889b045196840ee",
+    "url": "/division2builder/static/media/Borda.4a2b7158.ttf"
+  },
+  {
+    "revision": "dbaaa31d105b29d62adb208bc3bb79c1",
+    "url": "/division2builder/static/media/ChemLauncher.dbaaa31d.png"
+  },
+  {
+    "revision": "fa58903154f61fffed87d28610415a6b",
+    "url": "/division2builder/static/media/China.fa589031.png"
+  },
+  {
+    "revision": "67303e7b271e0f4173d8e1711c1050df",
+    "url": "/division2builder/static/media/Drone.67303e7b.png"
+  },
+  {
+    "revision": "5d2d8c091c5d81072ee45c6f1367000d",
+    "url": "/division2builder/static/media/Firefly.5d2d8c09.png"
+  },
+  {
+    "revision": "c46fbc384cb029d15c2eb26acf4d3e8a",
+    "url": "/division2builder/static/media/Gila.c46fbc38.png"
+  },
+  {
+    "revision": "d963d44164aeb6157b65e7a24d86954d",
+    "url": "/division2builder/static/media/HardWired.d963d441.png"
+  },
+  {
+    "revision": "ff77c4ab808ca31ccef6f20efb4fbc78",
+    "url": "/division2builder/static/media/Hive.ff77c4ab.png"
+  },
+  {
+    "revision": "0a0f1b3e31fdf5b176faebf75abab5c8",
+    "url": "/division2builder/static/media/OngoingDirective.0a0f1b3e.png"
+  },
+  {
+    "revision": "b6588a97ec82625d47637ff9a97690c5",
+    "url": "/division2builder/static/media/Overlord.b6588a97.png"
+  },
+  {
+    "revision": "b09fdb5d82ddfee4c0b3f9cddc32a803",
+    "url": "/division2builder/static/media/Pulse.b09fdb5d.png"
+  },
+  {
+    "revision": "dfb3a363692d864a0b6fa880723a7c4f",
+    "url": "/division2builder/static/media/SeekerMine.dfb3a363.png"
+  },
+  {
+    "revision": "bd6daa48c1a39bffd7fad62cc4e7a05d",
+    "url": "/division2builder/static/media/Shield.bd6daa48.png"
+  },
+  {
+    "revision": "912137c0b7fa4c25fab3f4b680ec4f2b",
+    "url": "/division2builder/static/media/Sokolov.912137c0.png"
+  },
+  {
+    "revision": "3b2bfbd4f7f5642295a8467617686a34",
+    "url": "/division2builder/static/media/TruePatriot.3b2bfbd4.png"
+  },
+  {
+    "revision": "e75ccaf753c1b00e545c6f3e326442a9",
+    "url": "/division2builder/static/media/Turret.e75ccaf7.png"
+  },
+  {
+    "revision": "775d7e065044a0b6cae660ed2d46399c",
+    "url": "/division2builder/static/media/background.775d7e06.png"
+  },
+  {
+    "revision": "a1d7134d7645931e65eb5ffe1cd8c908",
+    "url": "/division2builder/static/media/gunner.a1d7134d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/division2builder/precache-manifest.6f831a80188579980b5cf70fbe05427e.js"
+  "/division2builder/precache-manifest.f8901c589bbdeece2900ca20b3074fa0.js"
 );
 
 self.addEventListener('message', (event) => {
